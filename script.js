891197b8099d462b453008fefcd62430c6f6a43c
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-05-11 21:26:35 -0700

    updated position for button

--- script.js
+++ script.js
@@ -82,7 +82,7 @@ function toggleSidebar() {
     if (currentStyle === "0px") {
       $("#sidebar").css("width", "calc(100% - 35px)");
       $("#search-results", ".results-search-background").show();
-      $("#show-hide").css("left", "400px");
+      $("#show-hide").css("left", "0");
     } else {
       $("#sidebar").css("width", "0");
       $("#search-results", ".results-search-background").hide();
